b34c055087284b7179fb79c594ffd1afce67bd3f
vergil0327 <vergil@wangbaizhongdeMacBook-Air.local>
2022-09-01 00:27:50 +0800

    add graph.127 optimized example

--- Graphs/127. Word Ladder/main.go
+++ Graphs/127. Word Ladder/main.go
@@ -8,6 +8,62 @@ Output: 5
 Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
 */
 
+// explanation: https://www.youtube.com/watch?v=h9iTnkgv05E
+// T:O(nm^2), m for word length, n for wordList length
+// m < n, m <= 10 & n <= 5000
+// therefore, T:O(nm^2) is greater than T:O(mn^2)
+func ladderLengthOptimized(beginWord string, endWord string, wordList []string) int {
+	includeEndWord := false
+	for _, word := range wordList {
+		if word == endWord {
+			includeEndWord = true
+		}
+	}
+	if !includeEndWord {
+		return 0
+	}
+
+	graph := map[string][]string{}
+	for _, word := range append(wordList, beginWord) {
+		for j := range word {
+			pattern := word[:j] + "*" + word[j+1:]
+			graph[pattern] = append(graph[pattern], word)
+		}
+	}
+
+	visited := map[string]bool{}
+	queue := []string{beginWord}
+	count := 1 // already has beginWord
+	for len(queue) > 0 {
+		for _, word := range queue {
+			queue = queue[1:]
+
+			if word == endWord {
+				return count
+			}
+
+			if _, ok := visited[word]; ok {
+				continue
+			}
+
+			visited[word] = true
+
+			for j := range word {
+				pattern := word[:j] + "*" + word[j+1:]
+				for _, neighbor := range graph[pattern] {
+					if _, ok := visited[neighbor]; !ok {
+						queue = append(queue, neighbor)
+					}
+				}
+			}
+		}
+
+		count += 1
+	}
+
+	return 0
+}
+
 // think undirected graph
 func ladderLength(beginWord string, endWord string, wordList []string) int {
 	graph := buildGraph(append(wordList, beginWord))
@@ -17,7 +73,7 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
 	}
 
 	// bfs
-	count := 0
+	count := 1 // already one word exists
 	visited := map[string]bool{}
 	// predecessor := map[string]string{}
 	queue := []string{beginWord}
@@ -25,7 +81,6 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
 	// BFS: T:O(E+V), v stands for vertices & E for total number of edges in graph
 	// T:O(n^2*m), n^2 from iteration through all the edges, m for comparison between words
 	for len(queue) > 0 {
-		count += 1
 		for _, word := range queue {
 			// T:O(1) for deque item
 			queue = queue[1:]
@@ -49,6 +104,7 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
 				}
 			}
 		}
+		count += 1
 	}
 
 	return 0
