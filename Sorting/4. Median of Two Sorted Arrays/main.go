d39e2ee4c0e08816f8b182af76f9e58eed7bfc2e
vergil <vergil.wang@relajet.com>
2022-09-07 18:08:14 +0800

    feat: Sorting - 4. Median of Two Sorted Arrays - Binary Search Approach
    1. find mid1 of shorter length array & use total length - mid1 -2 to get mid2 of another array
    2. use A[mid1], A[mid1+1] & B[mid2], B[mid2+1] to check if we choose valid left portion of merged array and the other half
    3. median would be one or avg of these 4 value.
    P.S. make sure our comparison is valid by setting default value as math.MinInt & maht.MaxInt.
    Finally, make sure our binary search won't stuck in dead loop

---
+++ Sorting/4. Median of Two Sorted Arrays/main.go
@@ -0,0 +1,91 @@
+package main
+
+import "math"
+
+// https://www.geeksforgeeks.org/median-of-two-sorted-arrays-of-different-sizes/
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	panic("not_implement")
+}
+
+// https://www.youtube.com/watch?v=q6IEA26hvXc&ab_channel=NeetCode
+func findMedianSortedArraysBinarySearch(nums1 []int, nums2 []int) float64 {
+	var A, B []int
+	if len(nums1) < len(nums2) {
+		A = make([]int, len(nums1))
+		copy(A, nums1)
+		B = make([]int, len(nums2))
+		copy(B, nums2)
+	} else {
+		B = make([]int, len(nums1))
+		copy(B, nums1)
+		A = make([]int, len(nums2))
+		copy(A, nums2)
+	}
+
+	total := len(nums1) + len(nums2)
+	half := total / 2
+
+	// A is array with shorter length
+	l, r := 0, len(A)-1
+	for {
+		// l = midA + 1
+		// r = midA - 1
+		// if l,r = 0,1 => midA = 0
+		// midA = 0 + (1-0)/2 = 0 => dead loop
+		// midA = 1 - (1-0)/2 = 1 => l = 2, r = 0
+		midA := r - (r-l)/2
+
+		// why we substract by 2? -> convert lengths into indexes.
+		// E.x. A =  1 2 3 4 5 and B = 1 2 3 4 5
+		// A's midpoint would be index 2. We need midpoint in B to be index 1.
+		// So 5 (avg length) - midpointA - 2 = midpointB
+		midB := half - midA - 2
+
+		// use math.MinInt, math.MaxInt as default value for comparison afterwards
+		// ex. if A = [], B[1, 2, 3]; we want this comparison `Aleft <= Bright && Bleft <= Aright` is still be valid
+		Aleft, Aright := math.MinInt, math.MaxInt
+		if midA >= 0 {
+			Aleft = A[midA]
+		}
+		if midA+1 < len(A) {
+			Aright = A[midA+1]
+		}
+
+		Bleft, Bright := math.MinInt, math.MaxInt
+		if midB >= 0 {
+			Bleft = B[midB]
+		}
+		if midB+1 < len(B) {
+			Bright = B[midB+1]
+		}
+
+		// partition is correct
+		if Aleft <= Bright && Bleft <= Aright {
+			if isOdd := total/2 == 1; isOdd {
+				return float64(min(Aright, Bright))
+			} else {
+				return float64(max(Aleft, Bleft)+min(Aright, Bright)) / 2
+			}
+		} else if Aleft > Bright /* we need to shrink A size */ {
+			r = midA - 1
+		} else {
+			l = midA + 1
+		}
+	}
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+
+	return b
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+
+	return b
+}
