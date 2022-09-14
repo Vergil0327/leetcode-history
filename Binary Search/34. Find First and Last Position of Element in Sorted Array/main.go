8b0774cd881ed6623abc4bcbbdc8951b201c8aec
vergil0327 <ver9lovegakki@gmail.com>
2022-09-15 00:50:35 +0800

    feat: BinarySearch - 34. Find First and Last Position of Element in Sorted Array [Neetcode solution]
    we can write binary search in l <= r template and wrap as function to call.

--- Binary Search/34. Find First and Last Position of Element in Sorted Array/main.go
+++ Binary Search/34. Find First and Last Position of Element in Sorted Array/main.go
@@ -1,6 +1,35 @@
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 package main
 
+// leetcode: https://www.youtube.com/watch?v=4sQL7R5ySUU
+func searchRangeLeetcode(nums []int, target int) []int {
+	left := binSearch(nums, target, true)
+	right := binSearch(nums, target, false)
+	return []int{left, right}
+}
+
+func binSearch(nums []int, target int, leftBias bool) int {
+	i := -1
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := l + (r-l)/2
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			i = mid
+			if leftBias {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		}
+	}
+
+	return i
+}
+
 // T:O(logn)
 func searchRange(nums []int, target int) []int {
 	if len(nums) == 0 {
