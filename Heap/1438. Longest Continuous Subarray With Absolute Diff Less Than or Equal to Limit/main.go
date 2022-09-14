62691d81c44d588d831d95bd86cf1ca2efe62829
vergil <vergil.wang@relajet.com>
2022-09-14 17:33:40 +0800

    feat: Init 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit [Medium]

---
+++ Heap/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/main.go
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/submissions/
+package main
+
+func longestSubarray(nums []int, limit int) int {
+    
+}
