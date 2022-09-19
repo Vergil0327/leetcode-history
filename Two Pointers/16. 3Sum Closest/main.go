12c5a6eefde73e4707b935b35be8381fa99273b0
vergil <vergil.wang@relajet.com>
2022-09-19 10:55:49 +0800

    feat: TwoPointers - 16. 3Sum Closest [Medium]
    same as 3Sum and record diff extraly

--- Two Pointers/16. 3Sum Closest/main.go
+++ Two Pointers/16. 3Sum Closest/main.go
@@ -2,6 +2,40 @@
 
 package main
 
-func threeSumClosest(nums []int, target int) int {
+import (
+	"math"
+	"sort"
+)
 
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+
+	diff := math.MaxInt
+	candidate := 0
+	for i, num := range nums {
+		l, r := i+1, len(nums)-1
+		for l < r {
+			sum := num + nums[l] + nums[r]
+			if sum < target {
+				l += 1
+			} else if sum > target {
+				r -= 1
+			} else {
+				return target
+			}
+			if curr := Abs(sum - target); curr < diff {
+				diff = curr
+				candidate = sum
+			}
+		}
+	}
+
+	return candidate
+}
+
+func Abs(num int) int {
+	if num < 0 {
+		return -num
+	}
+	return num
 }
