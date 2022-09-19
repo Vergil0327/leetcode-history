bb153245f624562535c8594f04d2aaf4cfdf01eb
vergil0327 <ver9lovegakki@gmail.com>
2022-09-19 19:37:48 +0800

    feat: 18. 4Sum - add generic K-Sum solution
    we can use recursion to generalize solution for k-sum problem
    notices:
    1. remember to remove duplicate by moving pointer or skipping iteration
    2. for-loop only need to iteration until i<len(nums)-k+1 because we reserve nums as valid quadruplet
    3. just like backtracking, remember to remove quadrauplet after we've done current k-sum exploring

--- Two Pointers/18. 4Sum/main.go
+++ Two Pointers/18. 4Sum/main.go
@@ -6,11 +6,59 @@ import (
 )
 
 /*
-	neetcode: https://www.youtube.com/watch?v=EYeR-_1NRlQ&ab_channel=NeetCode
+generic k-Sum solution
+neetcode: https://www.youtube.com/watch?v=EYeR-_1NRlQ&ab_channel=NeetCode
 */
+func fourSum(nums []int, target int) [][]int {
+	sort.Ints(nums)
+
+	res, quadruplet := [][]int{}, []int{}
+
+	var kSum func(k, start, target int)
+	kSum = func(k, start, target int) {
+		if k > 2 {
+			// i<len(nums)-k+1: reserve valid quadruplets
+			for i := start; i < len(nums)-k+1; i++ {
+				// remove duplicate
+				if i > start && nums[i] == nums[i-1] {
+					continue
+				}
+
+				// just like backtracking
+				quadruplet = append(quadruplet, nums[i])
+				kSum(k-1, i+1, target-nums[i])
+				quadruplet = quadruplet[:len(quadruplet)-1]
+			}
+			return
+		}
+
+		// base case: 2Sum II
+		l, r := start, len(nums)-1
+		for l < r {
+			sum := nums[l] + nums[r]
+			if sum > target {
+				r -= 1
+			} else if sum < target {
+				l += 1
+			} else {
+				quad := append(quadruplet, nums[l], nums[r])
+
+				res = append(res, quad)
+				l += 1
+				// remove duplicate
+				for l < r && nums[l] == nums[l-1] {
+					l += 1
+				}
+			}
+		}
+	}
+
+	kSum(4, 0, target)
+	return res
+}
 
 // T:O(n^3)
-func fourSum(nums []int, target int) [][]int {
+func fourSum1stTry(nums []int, target int) [][]int {
 	// nlogn
 	sort.Ints(nums)
 
