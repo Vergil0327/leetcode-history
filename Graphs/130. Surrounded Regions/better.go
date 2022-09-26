c0237893b8b2f4da66fe7b07df7008434b08827c
vergil <vergil.wang@relajet.com>
2022-09-26 11:27:47 +0800

    feat: graph - 130. Surrounded Regions
    1. find invalid from border and marked. 2. flip valid 'O' and reverse marked one

--- Graphs/130. Surrounded Regions/better.go
+++ Graphs/130. Surrounded Regions/better.go
@@ -56,3 +56,60 @@ func solveCleverWay(board [][]byte) {
 		}
 	}
 }
+
+func solve20220926(board [][]byte) {
+	ROWS, COLS := len(board), len(board[0])
+
+	dirs := [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+	var dfs func(r, c int)
+	dfs = func(r, c int) {
+		if board[r][c] != 'O' {
+			return
+		}
+
+		if board[r][c] == 'O' {
+			board[r][c] = 'T' // tmp
+		}
+
+		for _, dir := range dirs {
+			dr, dc := dir[0], dir[1]
+			row, col := r+dr, c+dc
+
+			rowInBounds := row >= 0 && row < ROWS
+			colInBounds := col >= 0 && col < COLS
+			if rowInBounds && colInBounds {
+				dfs(row, col)
+			}
+		}
+	}
+
+	for r := 0; r < ROWS; r++ {
+		for c := 0; c < COLS; c++ {
+			if r == 0 {
+				dfs(r, c)
+			}
+
+			if r == ROWS-1 {
+				dfs(r, c)
+			}
+
+			if c == 0 {
+				dfs(r, c)
+			}
+
+			if c == COLS-1 {
+				dfs(r, c)
+			}
+		}
+	}
+
+	for r := 0; r < ROWS; r++ {
+		for c := 0; c < COLS; c++ {
+			if board[r][c] == 'T' {
+				board[r][c] = 'O'
+			} else if board[r][c] == 'O' {
+				board[r][c] = 'X'
+			}
+		}
+	}
+}
