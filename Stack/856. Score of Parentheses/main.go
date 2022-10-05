df3307c31634081e26334fb7412fc0acd09fa7a5
vergil0327 <ver9lovegakki@gmail.com>
2022-10-06 03:29:40 +0800

    feat: we can also use recursion to get scoreOfParentheses

--- Stack/856. Score of Parentheses/main.go
+++ Stack/856. Score of Parentheses/main.go
@@ -1,5 +1,33 @@
 package main
 
+// https://www.youtube.com/watch?v=tiAaVfMcL9w
+func scoreOfParenthesesRecursive(s string) int {
+	var score func(l, r int) int
+	score = func(l, r int) int {
+		if r-l == 1 { // one ()
+			return 1
+		}
+
+		balance := 0
+		for i := l; i < r; i++ {
+			if s[i] == '(' {
+				balance += 1
+			} else {
+				balance -= 1
+			}
+			if balance == 0 { // () + remaining string
+				return score(l, i) + score(i+1, r)
+			}
+		}
+
+		// if we don't see any balance parentheses happening inside for-loop
+		// then it must be outer parentheses
+		return 2 * score(l+1, r-1)
+	}
+
+	return score(0, len(s)-1)
+}
+
 // https://www.youtube.com/watch?v=fAsV6SIJ-GI
 func scoreOfParentheses(s string) int {
 	stack := []int{}
