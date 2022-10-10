d763d9b28628c25f7272bffa63dad16c574797a0
vergil0327 <ver9lovegakki@gmail.com>
2022-10-10 11:03:38 +0800

    feat: add codeforces easy implementation of segment tree

--- segmentTree.go
+++ segmentTree.go
@@ -1,3 +1,5 @@
+// CodeForce: https://codeforces.com/blog/entry/18051
+
 // https://www.geeksforgeeks.org/iterative-segment-tree-range-minimum-query/
 // The iterative version of the segment tree:
 // for an index i, left child = 2 * i and right child = 2 * i + 1 in the tree.
@@ -20,6 +22,7 @@ func NewSegmentTree(n int) SegmentTree {
 // compute the minimum at each height change
 // the index to leaf node first
 // T:O(log(n))
+// query [l, r), right exclusive
 func (t *SegmentTree) Query(l, r int) int {
 	// leaf node
 	l += t.n
@@ -33,7 +36,8 @@ func (t *SegmentTree) Query(l, r int) int {
 			l += 1
 		}
 
-		// if right index is odd (if it's even, it's covered by next higher level, i.e. it's parent node)
+		// if right index is odd, it's right child node
+		// we include r-1 (left child node) and keep finding
 		if r&1 == 1 {
 			r -= 1
 			ans = int(math.Max(float64(ans), float64(t.tree[r])))
@@ -57,4 +61,69 @@ func (t *SegmentTree) Update(i, val int) {
 		// update the values in the nodes in the next higher level
 		t.tree[i] = int(math.Max(float64(t.tree[i*2]), float64(t.tree[i*2+1])))
 	}
+}
+
+/* code force: https://codeforces.com/blog/entry/18051 */
+
+type SumSegmentTree struct {
+	n    int
+	tree []int
+}
+
+func (seg *SumSegmentTree) Build(nums []int) {
+	seg.n = len(nums)
+	seg.tree = make([]int, 2*seg.n)
+
+	// 1-based
+	for i := seg.n - 1; i > 0; i-- {
+		seg.tree[i] = seg.tree[i<<1] + seg.tree[i<<1|1]
+
+		// or
+		// seg.tree[i] = seg.tree[2*i] + seg.tree[2*i+1]
+	}
+}
+
+// set value at position i
+func (seg *SumSegmentTree) Update(i int, val int) {
+	// shift to leaf node
+	i += seg.n
+	seg.tree[i] = val
+
+	// for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
+	for i > 1 {
+		seg.tree[i>>1] = seg.tree[i] + seg.tree[i^1]
+		i >>= 1
+	}
+
+	// or
+	// for i > 1 {
+	// 	i >>= 1
+	// 	seg.tree[i] = seg.tree[i*2] + seg.tree[i*2+1]
+	// }
+}
+
+// query [l, r), 左閉右開
+// if we want 3-10 -> Query(3, 10+1)
+// see picture for better explanation: https://i.imgur.com/GGBmcEP.png
+func (seg *SumSegmentTree) Query(l, r int) int {
+	res := 0
+	l, r = l+seg.n, r+seg.n
+
+	for l < r {
+		// right node in left subtree
+		if l&1 == 1 {
+			res += seg.tree[l] // include it
+			l += 1
+		}
+
+		// right node in right subtree
+		if r&1 == 1 {
+			r -= 1             // become left node in right subtree
+			res += seg.tree[r] // include it
+		}
+
+		l >>= 1
+		r >>= 1
+	}
+	return res
 }
