930da8d607a367e1431cb735fd8640d559020c04
vergil <vergil.wang@relajet.com>
2022-10-11 18:13:56 +0800

    feat: add better implementation to 876. Middle of the Linked List

--- Linked List/876. Middle of the Linked List/main.go
+++ Linked List/876. Middle of the Linked List/main.go
@@ -9,6 +9,20 @@ type ListNode struct {
 	Next *ListNode
 }
 
+func middleNodeBetter(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
+
 // recommended by https://zhuanlan.zhihu.com/p/349940945
 func middleNode(head *ListNode) *ListNode {
 	slow, fast := head, head
