07d03c53b51f9437f4cbeed9f430edcbfd52bec3
vergil0327 <ver9lovegakki@gmail.com>
2022-10-12 19:42:27 +0800

    feat: DP - 322. Coin Change - add top-down solution

--- 1-D Dynamic Programming/322. Coin Change/main.go
+++ 1-D Dynamic Programming/322. Coin Change/main.go
@@ -47,6 +47,92 @@ func coinChange(coins []int, amount int) int {
 	}
 }
 
+// https://leetcode.com/problems/coin-change/discuss/1475250/Python-4-solutions%3A-Top-down-DP-Bottom-up-DP-Space-O(amount)-Clean-and-Concise
+func coinChangeTopDown(coins []int, amount int) int {
+	memo := map[[2]int]int{}
+	var dfs func(i, amount int) int
+	dfs = func(i, amount int) int {
+		if amount == 0 {
+			memo[[2]int{i, amount}] = 0
+			return 0
+		}
+
+		if i >= len(coins) {
+			memo[[2]int{i, amount}] = math.MaxInt
+			return math.MaxInt
+		}
+
+		if _, ok := memo[[2]int{i, amount}]; ok {
+			return memo[[2]int{i, amount}]
+		}
+
+		ans := dfs(i+1, amount) // skip
+
+		if amount >= coins[i] { // used
+			v := dfs(i, amount-coins[i])
+			if v != math.MaxInt {
+				// if v == math.MaxInt, v+1 will overflow
+				ans = min(ans, v+1)
+			}
+		}
+
+		memo[[2]int{i, amount}] = ans
+		return ans
+	}
+
+	if count := dfs(0, amount); count == math.MaxInt {
+		return -1
+	} else {
+		return count
+	}
+}
+
+// we don't really cache index of coin we used
+func coinChangeTopDownOptimized(coins []int, amount int) int {
+	memo := map[int]int{}
+
+	var dfs func(amount int) int
+	dfs = func(amount int) int {
+		if amount == 0 {
+			return 0
+		}
+
+		if _, ok := memo[amount]; ok {
+			return memo[amount]
+		}
+
+		used := math.MaxInt
+		for _, coin := range coins {
+			// prune the invalid choices
+			if amount >= coin {
+				if count := dfs(amount - coin); count != math.MaxInt {
+					used = min(used, 1+count)
+				}
+			}
+		}
+
+		memo[amount] = used
+		return used
+	}
+
+	if used := dfs(amount); used == math.MaxInt {
+		return -1
+	} else {
+		return used
+	}
+}
+
+func min(nums ...int) int {
+	min := math.MaxInt
+	for _, v := range nums {
+		if v < min {
+			min = v
+		}
+	}
+
+	return min
+}
+
 // explanation: https://www.youtube.com/watch?v=H9bfqozjoqs
 func coinChangeOtherDP(coins []int, amount int) int {
 	dp := make([]int, amount+1)
