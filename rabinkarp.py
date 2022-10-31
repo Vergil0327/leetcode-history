8243392f26fbbb75320f9d1229074625a7d94cd4
vergil0327 <ver9lovegakki@gmail.com>
2022-10-31 23:14:06 +0800

    feat: add Rabin-Karp algorithm impl

---
+++ rabinkarp.py
@@ -0,0 +1,42 @@
+# explanation: https://labuladong.github.io/algo/2/20/28/
+
+# return index of matched substring
+def rabinKarp(txt: str, pattern: str) -> int:
+    # 位數
+    L = len(pattern)
+
+    # 進位制
+    R = 256 # ascii code (256 characters)
+
+    # 大數 質數 求餘數
+    Q = 1658598167
+
+    # R ** (L-1)
+    RL = 1
+    for i in range(len(txt)):
+        RL = (RL * R) % Q # avoid overflow
+    
+    # construct pattern's hash
+    patHash = 0
+    for i in range(len(pattern)):
+        patHash = (R * patHash + ord(pattern[i])) % Q
+
+    windowHash = 0
+    l, r = 0, 0
+    while r < len(txt):
+        # (X+Y) % Q = (X%Q + Y%Q) % Q
+        # X % Q = (X+Q) % Q
+        windowHash = ((R * windowHash) % Q + ord(txt[r])) % Q
+        r += 1
+
+        if r-l == L:
+            if windowHash == patHash:
+                # prevent hash collision
+                if txt[l:r] == pattern:
+                    return l
+        
+            # because windowHash - (ord(txt[l]) * RL)%Q can be negative, we plus Q to make sure windowHash is positive
+            windowHash = (windowHash - (ord(txt[l]) * RL)%Q + Q) % Q
+            l += 1
+    
+    return -1 # if not found
