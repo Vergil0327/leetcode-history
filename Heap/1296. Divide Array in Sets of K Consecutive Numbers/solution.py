1df58c21330c55be33353493bf41e31f5fe0255b
vergil0327 <ver9lovegakki@gmail.com>
2022-11-02 20:33:58 +0800

    feat: add optimal solution to 1296. Divide Array in Sets of K Consecutive Numbers

--- Heap/1296. Divide Array in Sets of K Consecutive Numbers/solution.py
+++ Heap/1296. Divide Array in Sets of K Consecutive Numbers/solution.py
@@ -23,3 +23,28 @@ class Solution:
                 if counter[num] == 0:
                     heapq.heappop(minH)
         return True
+
+# O(n)
+# https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/discuss/457728/O(n)-Python-and-C%2B%2B
+class SolutionOptimized:
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+        if len(nums)%k != 0: return False
+        if k == 1: return True
+        
+        # counter = collections.Counter(nums)
+        counter = defaultdict(lambda: 0)
+        for num in nums:
+            counter[num] = counter.get(num, 0) + 1
+
+        for num in nums:
+            start = num
+            while counter[start-1]:
+                start -= 1
+            while start <= num:
+                while counter[start]:
+                    for nextNum in range(start, start+k):
+                        if counter[nextNum] == 0:
+                            return False
+                        counter[nextNum] -= 1
+                start += 1
+        return True
