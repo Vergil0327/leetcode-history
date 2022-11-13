6a7589a9147576292574b908087b04157395058c
vergil0327 <ver9lovegakki@gmail.com>
2022-11-14 03:24:15 +0800

    feat: 2472. Maximum Number of Non-overlapping Palindrome Substrings - add bottom-up DP

--- 1-D Dynamic Programming/2472. Maximum Number of Non-overlapping Palindrome Substrings/dp_solution.py
+++ 1-D Dynamic Programming/2472. Maximum Number of Non-overlapping Palindrome Substrings/dp_solution.py
@@ -56,3 +56,28 @@ class SolutionTLE:
 
             return cnt
         return dfs(0)
+
+class Solution:
+    @functools.lru_cache(None)
+    def isPal(self, s, l, r):
+        if r >= len(s) or s[l] != s[r]: return False
+        
+        if l < r:
+            return self.isPal(s, l+1, r-1)
+        else:
+            return True
+
+    def maxPalindromes(self, s: str, k: int) -> int:
+        n = len(s)
+        
+        # dp[i]: the maximum number of substrings s[i:]
+        dp = [0]* (n+1)
+        for i in range(k, n+1):
+            dp[i] = dp[i-1]
+            if self.isPal(s, i-k, i-1):
+                dp[i] = max(dp[i], 1+ dp[i-k])
+            # i-k-1 >= 0 avoid out-of-bound
+            if i-k-1 >=0 and self.isPal(s, i-k-1, i-1):
+                dp[i] = max(dp[i], 1+ dp[i-k-1])
+
+        return dp[n]
