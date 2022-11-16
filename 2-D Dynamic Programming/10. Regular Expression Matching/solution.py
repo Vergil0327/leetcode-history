9837bfb730500ab7db6b7c48b397f122dbe48741
vergil0327 <ver9lovegakki@gmail.com>
2022-11-17 02:57:50 +0800

    feat: add concise bottom-up solution to 10. regular expressing matching

--- 2-D Dynamic Programming/10. Regular Expression Matching/solution.py
+++ 2-D Dynamic Programming/10. Regular Expression Matching/solution.py
@@ -70,3 +70,43 @@ class Solution:
                         dp[i][j] = False
         
         return dp[m][n]
+
+# Concise Solution, using isalpha()
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        
+        # dp[i][j] : matching result where p[:j] cover the entire s[:i]
+        dp = [[False] * (n+1) for _ in range(m+1)]
+        # easier to handle index in for-range iteration if we append placeholder to shift index.
+        s = "#" + s
+        p = "#" + p
+        
+        # Base Case
+        dp[0][0] = True
+        # dp[i][0] = False, where i from 0 to m. s[:i] = "XXXX" and p[:j] = ""
+        for j in range(1, n+1, 2):
+            if j+1<n+1 and p[j+1] == "*":
+                dp[0][j] = True
+                dp[0][j+1] = True
+            else:
+                break
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if p[j].isalpha() or p[j] == ".": # just string matching
+                    if s[i] == p[j] or p[j] == ".":
+                        dp[i][j] = dp[i-1][j-1]
+
+                # change order
+                # since problem guarantee pattern is always valid,
+                # we don't need to consider out-of-bound index error
+                # ! use elif p[j] == "*" not else
+                elif p[j] == "*":
+                    # choose NOT to use * to match s[i]
+                    dp[i][j] = dp[i][j-2]
+                    
+                    # use * with preceding character to match s[i]
+                    dp[i][j] = dp[i][j] or (dp[i-1][j] if s[i] == p[j-1] or p[j-1] == "." else False)
+        
+        return dp[m][n]
