afa24eb8e27fa69de437aac2ec46aced5227b605
vergil <vergil.wang@relajet.com>
2022-11-25 11:22:49 +0800

    feat: MonoStack - 907. Sum of Subarray Minimums - add standard 3 pass solution

--- Stack/Monotone Stack/907. Sum of Subarray Minimums/solution.py
+++ Stack/Monotone Stack/907. Sum of Subarray Minimums/solution.py
@@ -14,3 +14,31 @@ class Solution:
             stack.append(i)
             
         return res%mod
+
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        n = len(arr)
+        MOD = int(1e9+7)
+
+        nextSmaller = [n] * n
+        stack = []
+        for i, num in enumerate(arr):
+            while stack and arr[stack[-1]] > num:
+                nextSmaller[stack.pop()] = i
+            stack.append(i)
+
+        prevSmaller = [-1] * n
+        stack.clear()
+        for i in range(n-1, -1, -1):
+            # ! important to consider duplicate num
+            # ex. [3,1,2,2,2,2,4], use >= to find correct prev smaller including equal number
+            while stack and arr[stack[-1]] >= arr[i]:
+                prevSmaller[stack.pop()] = i
+            stack.append(i)
+
+        total = 0
+        for i, num in enumerate(arr):
+            # num as minimum
+            # # of nums <= num in left * # of nums < num in right
+            total += num * ((i-prevSmaller[i]) * (nextSmaller[i]-i)) % MOD
+        return total % MOD
