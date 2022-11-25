efbe3a94d4f59fed5c2c0c8bb8add3a00abb9910
vergil0327 <ver9lovegakki@gmail.com>
2022-11-25 16:00:31 +0800

    feat: 912. Sort an Array - shell sort

--- Sorting/912. Sort an Array/shellsort.py
+++ Sorting/912. Sort an Array/shellsort.py
@@ -1,3 +1,10 @@
+# Pseudo Code
+# shellSort(array, size)
+        #   for interval i <- size/2n down to 1
+        #     for each interval "i" in array
+        #         sort all the elements at interval "i"
+        # end shellSort
+
 # shell sort is optimized insertion sort: https://www.programiz.com/dsa/shell-sort
 # time complexity
 # Best	O(nlog n)
@@ -10,18 +17,17 @@ class Solution:
         def shellsort(nums, n):
             # start with a big gap, then reduce the gap
             gap = n//2
-            while gap >= 1:
+            while gap > 0:
                 # repeat insertion sort with different gaps 
                 # Code exactly similar to insertion sort, only difference is that in case of insertion sort there is only `gap == 1`
                 # but here gap start from n/2 and keep reducing untill it becomes 0.
                 for i in range(gap, n):
-                    key = nums[i]
+                    current = nums[i]
                     j = i - gap
-                    while j >= 0 and nums[j] > key:
+                    while j >= 0 and nums[j] > current:
                         nums[j+gap] = nums[j]
                         j -= gap
-                    nums[j+gap] = key
+                    nums[j+gap] = current
                 gap //= 2
-        
-        shellsort(nums, len(nums))
-        return nums
+            return nums
+        return shellsort(nums, len(nums))
