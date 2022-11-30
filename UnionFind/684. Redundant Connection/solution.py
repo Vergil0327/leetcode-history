a76ad384b068a6e94e79ae69c74d023dd093ce39
vergil0327 <ver9lovegakki@gmail.com>
2022-12-01 00:03:49 +0800

    feat: UnionFind - 684. Redundant Connection

---
+++ UnionFind/684. Redundant Connection/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        parent = [i for i in range(len(edges)+1)]
+        
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x, y):
+            px, py = find(x), find(y)
+            if px == py: return False
+            
+            parent[py] = parent[px]
+            return True
+        
+        for u, v in edges:
+            if not union(u, v): return [u, v]
