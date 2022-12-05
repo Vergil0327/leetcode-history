a0c94cef97b5803ea442da5cecd680e395cf3991
vergil0327 <ver9lovegakki@gmail.com>
2022-12-05 21:50:23 +0800

    feat: Backtracking - 46. Permutations

--- Backtracking/46. Permutations/main.go
+++ Backtracking/46. Permutations/main.go
@@ -1,21 +1,6 @@
 // https://leetcode.com/problems/permutations/
 package main
 
-// Example 1:
-// Input: nums = [1,2,3]
-// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-//  1         2         3
-// 2, 3			 1, 3		 	 1, 2
-// 3,  2    3 ,  1 		2			1
-
-// Example 2:
-// Input: nums = [0,1]
-// Output: [[0,1],[1,0]]
-
-// Example 3:
-// Input: nums = [1]
-// Output: [[1]]
-
 // https://www.youtube.com/watch?v=s7AvT7cGdSo
 // https://www.youtube.com/watch?v=CUzm-buvH_8&t=479s
 func permute(nums []int) [][]int {
---
+++ Backtracking/46. Permutations/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        visited = set()
+        def dfs(state):
+            if len(state) == len(nums):
+                res.append(state.copy())
+                return
+
+            for num in nums:
+                if num in visited: continue
+                state.append(num)
+                visited.add(num)
+                dfs(state)
+                state.pop()
+                visited.remove(num)
+        dfs([])
+        return res
