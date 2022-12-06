f878f2f779bea4775bcefec428c413f3a9a9d54e
vergil0327 <ver9lovegakki@gmail.com>
2022-12-06 21:07:20 +0800

    feat: 46. Permutations - add bitmask solution

--- Backtracking/46. Permutations/solution.py
+++ Backtracking/46. Permutations/solution.py
@@ -18,7 +18,6 @@ class Solution:
         return res
 
 
-
 class SmartSolution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
@@ -30,4 +29,27 @@ class SmartSolution:
             for i, num in enumerate(nums):
                 dfs(state+[num], nums[:i] + nums[i+1:])
         dfs([], nums)
+        return res
+
+# Bitmask
+# since nums.length <= 6, we can use 6 bit bitmast to replace `visited` hashset
+# it's even more efficient than using array as `visited`
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+        
+        def dfs(state, bitmask):
+            if len(state) == len(nums):
+                res.append(state.copy())
+                return
+
+            for i, num in enumerate(nums):
+                if 1<<i & bitmask: continue
+                state.append(num)
+                bitmask |= 1<<i
+                dfs(state, bitmask)
+                state.pop()
+                bitmask ^= 1<<i
+        dfs([], 0)
         return res
