6dba612f17be03a2d069bbc57f9681d8d64ad285
vergil0327 <ver9lovegakki@gmail.com>
2022-12-09 22:54:20 +0800

    docs: add inline comment in 204. Count Primes

--- Math&Geometry/204. Count Primes/solution.py
+++ Math&Geometry/204. Count Primes/solution.py
@@ -5,8 +5,17 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         isPrime = [0, 0] + [1] * (n-2)
 
+        # take 12 as example
+        # 12 = 2 × 6
+        # 12 = 3 × 4
+        # 12 = sqrt(12) × sqrt(12)
+        # 12 = 4 × 3
+        # 12 = 6 × 2
+        # we can see that all the factors after sqrt(12) x sqrt(12) have been considered
+        # thus, we only need to iterate until sqrt(n)+1 (strictly less than)
         for i in range(2, int(math.sqrt(n))+1):
             if not isPrime[i]: continue
+            # all the factors before i*i have been considered at i-1, i-2, ..., 3, 2
             for j in range(i*i, n, i):
                 isPrime[j] = 0
         return sum(isPrime)
