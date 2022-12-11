80a8c6f2aa255a967b7c82968a044a3fdcffa689
vergil0327 <ver9lovegakki@gmail.com>
2022-12-11 14:39:27 +0800

    feat: 124. Binary Tree Maximum Path Sum - add other solution

--- Trees/124. Binary Tree Maximum Path Sum/solution.py
+++ Trees/124. Binary Tree Maximum Path Sum/solution.py
@@ -13,3 +13,19 @@ class Solution:
 
         dfs(root)
         return maxSum
+
+# Without Global Variable
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if not root: return 0, -inf
+
+            left, left_max_path_sum = dfs(root.left)
+            right, right_max_path_sum = dfs(root.right)
+            curr_max_path_sum = root.val + max(0, left) + max(0, right)
+            maxSum = max(left_max_path_sum, right_max_path_sum, curr_max_path_sum)
+
+            return root.val + max(left, right, 0), maxSum
+
+        _, maxSum = dfs(root)
+        return maxSum
