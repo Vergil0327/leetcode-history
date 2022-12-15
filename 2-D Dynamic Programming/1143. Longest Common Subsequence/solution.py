7693b5228827026d61b053284829ddd4f3f41e3b
vergil <vergil.wang@relajet.com>
2022-12-15 10:19:04 +0800

    feat: 2-D DP - 1143 LCS - add python solution with 1-indexed trick

--- 2-D Dynamic Programming/1143. Longest Common Subsequence/solution.py
+++ 2-D Dynamic Programming/1143. Longest Common Subsequence/solution.py
@@ -33,3 +33,19 @@ class Solution:
                     
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         return dp[m][n]
+
+# we can append dummy word in front of text1 and text2 to make `i`, `j` pointer consistent with
+# we can get rid of text1[i-1], text2[j-1]
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        text1 = "#"+text1
+        text2 = "#"+text2
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+        return dp[m][n]
