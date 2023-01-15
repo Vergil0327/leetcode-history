b972a959174357c92c0b8345bb9d30dce78f199e
vergil0327 <ver9lovegakki@gmail.com>
2023-01-15 21:39:11 +0800

    feat: 407. Trapping Rain Water II - add DFS + Min Heap solution

--- Graphs/Dijkstra/407. Trapping Rain Water II/INTUITION.md
+++ Graphs/Dijkstra/407. Trapping Rain Water II/INTUITION.md
@@ -1,4 +1,5 @@
-# Intuition
+# BFS + MinHeap (Dijkstra)
+## Intuition
 
 這題最關鍵的思路是**從邊界著手**，外圍一圈邊界湧入海水後，如果內部更低則會填滿
 因此我們可以從最低的邊界開始持續湧入
@@ -11,7 +12,7 @@
 每當從min heap裡取出一個，便更新海平面到最新的高度 `seaLevel or wallHeight`，此時往四個方向進行BFS，能每一格能填充的水即為 `seaLevel/wallHeight - heightMap[current cell]`
 持續加總即得最終能困住的水
 
-# Complexity
+## Complexity
 
 - time complexity
 
@@ -23,3 +24,15 @@ $$O(mnlog(mn) + mnlog(mn))$$
 
 - space complexity
 $$O(n)$$
+
+# DFS + MinHeap
+
+## Intuition
+
+DFS思路是一樣的，其實這題重點就是從邊界由低到高找出能困住的水
+一但往內搜索找出無法再繼續湧入時，代表我們又找得了新的邊界，持續加入到`min heap`裡
+直到全部邊界搜索完畢，便也找出所有被困住的水了
+
+以DFS搜索效率會再快一點，這是因為在往低處搜索時是常規的DFS，持續往四周搜索，以O(4)時間找出四個方向
+
+而上面的BFS+MinHeap解法則會每次都從`min heap`裡找出接下來要搜索的四個方向，這會是4log(mn)的時間找出四個方向
--- Graphs/Dijkstra/407. Trapping Rain Water II/solution.py
+++ Graphs/Dijkstra/407. Trapping Rain Water II/solution.py
@@ -1,3 +1,4 @@
+# BFS + Min Heap
 class Solution:
     def trapRainWater(self, heightMap: List[List[int]]) -> int:
         m, n = len(heightMap), len(heightMap[0])
@@ -28,4 +29,39 @@ class Solution:
                 if (heightMap[nxtY][nxtX], nxtY, nxtX) in visited: continue
                 heapq.heappush(minH, [heightMap[nxtY][nxtX], nxtY, nxtX])
 
+        return trapped
+
+# DFS + minHeap
+class Solution:
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        m, n = len(heightMap), len(heightMap[0])
+        
+        border = [] # [height, x, y]
+        for y in range(m):
+            for x in range(n):
+                if y == 0 or y == m-1 or x == 0 or x == n-1:
+                    heapq.heappush(border, [heightMap[y][x], y, x])
+
+        wallHeight = 0
+        visited = set()
+        dirs = [[0,1],[0,-1],[1,0],[-1,0]]
+        def dfs(y, x, wallHeight):
+            if (y, x) in visited: return 0
+            visited.add((y,x))
+
+            water = wallHeight - heightMap[y][x]
+            for dy, dx in dirs:
+                nxtY, nxtX = y+dy, x+dx
+                if not (nxtY >= 0 and nxtY < m and nxtX >= 0 and nxtX < n): continue
+                if heightMap[nxtY][nxtX] < wallHeight:
+                    water += dfs(nxtY, nxtX, wallHeight)
+                else:
+                    heapq.heappush(border, [heightMap[nxtY][nxtX], nxtY, nxtX])
+            return water
+
+        trapped = 0
+        while border:
+            hei, y, x = heapq.heappop(border)
+            wallHeight = max(wallHeight, hei)
+            trapped += dfs(y, x, wallHeight)
         return trapped
