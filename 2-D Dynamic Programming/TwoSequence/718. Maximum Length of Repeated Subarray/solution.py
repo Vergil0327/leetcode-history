ef65f079119e3573ac73508d74b9c1c90548e63a
vergil0327 <vergil.wang@relajet.com>
2023-02-20 18:19:08 +0800

    feat: TwoSeq. - 718. Maximum Length of Repeated Subarray

--- 2-D Dynamic Programming/TwoSequence/718. Maximum Length of Repeated Subarray/solution.py
+++ 2-D Dynamic Programming/TwoSequence/718. Maximum Length of Repeated Subarray/solution.py
@@ -26,6 +26,24 @@ class Solution:
                 #     dp[i][j] = 0
         return max(map(max, dp))
 
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        m, n = len(nums1), len(nums2)
+
+        dp = [[0]*(n+1) for _ in range(m+1)]
+
+        # manually shift to 1-indexed
+        nums1 = [0] + nums1
+        nums2 = [0] + nums2
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if nums1[i] == nums2[j]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                # else:
+                #     dp[i][j] = 0
+        return max(map(max, dp))
+
 
 """
 time complexity: O(mn)
