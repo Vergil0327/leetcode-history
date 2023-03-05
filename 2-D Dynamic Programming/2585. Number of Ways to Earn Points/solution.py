b2326ab01f6f70f4643a4eeab425e2585e7bf15b
vergil0327 <ver9lovegakki@gmail.com>
2023-03-05 14:08:16 +0800

    feat: 2-D DP - 2585. Number of Ways to Earn Points

---
+++ 2-D Dynamic Programming/2585. Number of Ways to Earn Points/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def waysToReachTarget(self, target: int, types: List[List[int]]) -> int:
+        MOD = int(1e9+7)
+
+        n = len(types)
+
+        # dp[i][j]: the number of ways you can earn exactly j points in the exam considering types[:i]
+        dp = [[0]*(target+1) for _ in range(n+1)]
+        dp[0][0] = 1
+
+        types = [[-1, -1]] + types
+        for i in range(1, n+1):
+            count, mark = types[i][0], types[i][1]
+            for j in range(target+1):
+                for cnt in range(count+1):
+                    dp[i][j] += dp[i-1][j-cnt*mark] if j-cnt*mark>=0 else 0
+                    dp[i][j] %= MOD
+
+        return dp[n][target] % MOD
