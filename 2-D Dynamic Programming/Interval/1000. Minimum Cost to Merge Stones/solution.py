6900c3198ae955269ebac53b1e9a98f6d71e5232
vergil0327 <vergil.wang@relajet.com>
2023-04-19 15:33:33 +0800

    add solution to 1000. Minimum Cost to Merge Stones

--- 2-D Dynamic Programming/Interval/1000. Minimum Cost to Merge Stones/solution.py
+++ 2-D Dynamic Programming/Interval/1000. Minimum Cost to Merge Stones/solution.py
@@ -30,3 +30,30 @@ class Solution:
                     dp[i][j][1] = dp[i][j][K] + presum[j+1] - presum[i]
         
         return dp[0][n-1][1]
+    
+
+class Solution:
+    def mergeStones(self, stones: List[int], K: int) -> int:
+        n = len(stones)
+
+        if (n-1)%(K-1) != 0: return -1
+
+        presum = [0] * (n+1)
+        for i in range(1, n+1):
+            presum[i] = presum[i-1] + stones[i-1]
+
+        dp = [[0]*n for _ in range(n)]
+        for length in range(K, n+1):
+            for i in range(n-length+1): # j=i+length-1 < n
+                j = i+length-1
+                dp[i][j] = inf
+                for mid in range(i, j, K-1):
+                    dp[i][j] = min(dp[i][j], dp[i][mid]+dp[mid+1][j])
+
+                # Every time len-1 is a multiple of K, we incur a cost.
+                # This cost = sum of ( all stones under the span of len).
+                # Since the cost is merely sum of a contiguos sub-array of input, we pre-caculate the partial sums for O(1) lookup.
+                # The check (len-1 )% (K-1) == 0, signals a true merge and we calcuate the additional costs for this sub-problem.
+                if (j-i)%(K-1) == 0:
+                    dp[i][j] += presum[j+1]-presum[i]
+        return dp[0][n-1]
