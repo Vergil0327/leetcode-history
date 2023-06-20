4ef285fd802b153c32fe7a5f1994b5373c1939ec
vergil0327 <vergil.wang@relajet.com>
2023-06-20 14:56:36 +0800

    feat: mv 1871. Jump Game VII to diff array

--- PrefixSum/DiffSum/1871. Jump Game VII/solution.py
+++ PrefixSum/DiffSum/1871. Jump Game VII/solution.py
@@ -23,3 +23,23 @@ class Solution:
                 diff[i+maxJump+1] -= 1 # mark ending point of reachable interval
         
         return canReach > 0
+    
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        n = len(s)
+        diff = [0] * (n+maxJump+1)
+        
+        diff[minJump] += 1
+        diff[maxJump+1] -= 1
+
+        for i in range(1, n):
+            diff[i] += diff[i-1]
+            
+            if s[i] == "1": continue # can't jump on it
+            if diff[i] == 0: continue # can't reach
+
+            # mark reach interval
+            diff[i+minJump] += 1
+            diff[i+maxJump+1] -= 1
+
+        return diff[n-1] > 0 and s[n-1] == "0"
