d8e91a4035e00467fcd5988a7870f70619901a77
vergil0327 <vergil.wang@relajet.com>
2023-08-10 15:12:50 +0800

    feat: add other implementation of 1626. Best Team With No Conflicts

--- 1-D Dynamic Programming/1626. Best Team With No Conflicts/solution.py
+++ 1-D Dynamic Programming/1626. Best Team With No Conflicts/solution.py
@@ -12,3 +12,18 @@ class Solution:
                 elif arr[j][0] == baseAge:
                     dp[i] = max(dp[i], dp[j] + score)
         return max(dp)
+
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        players = sorted(zip(ages, scores))
+
+        n = len(players)
+        dp = [0] * n
+        for i in range(n):
+            dp[i] = max(dp[i], players[i][1])
+            for j in range(i):
+                # continue if conflict
+                if players[j][0] < players[i][0] and players[j][1] > players[i][1]: continue
+                
+                dp[i] = max(dp[i], dp[j] + players[i][1])
+        return max(dp)
