0e87af21ced92927aa43ad71aa093a845ca610cf
vergil0327 <vergil.wang@relajet.com>
2023-11-20 15:20:24 +0800

    feat: add top-down dp - 1125. Smallest Sufficient Team

--- 2-D Dynamic Programming/Bitmask/1125. Smallest Sufficient Team/solution.py
+++ 2-D Dynamic Programming/Bitmask/1125. Smallest Sufficient Team/solution.py
@@ -25,3 +25,39 @@ class Solution:
                     dp[next_state] = dp[state]+1
                     res[next_state] = res[state] | {i}
         return list(res[(1<<n)-1])
+    
+# top-down
+class Solution:
+    def smallestSufficientTeam(self, req_skills: List[str], people: List[List[str]]) -> List[int]:
+        sk2idx = {}
+        for i, skill in enumerate(req_skills):
+            sk2idx[skill] = i
+
+        arr = []
+        for skills in people:
+            state = 0
+            for sk in skills:
+                j = sk2idx[sk]
+                state |= 1<<j
+            arr.append(state)
+        m = len(req_skills)
+        n = len(arr)
+
+        @cache
+        def dfs(i, state):
+            if i == n:
+                return [] if state == (1<<m)-1 else None
+
+            x = dfs(i+1, state)
+            y = dfs(i+1, state|arr[i])
+
+            if x is None and y is None:
+                return None
+            elif x is None:
+                return y + [i]
+            elif y is None:
+                return x
+            else:
+                return x if len(x) < len(y)+1 else y + [i]
+        return dfs(0, 0)
+                
