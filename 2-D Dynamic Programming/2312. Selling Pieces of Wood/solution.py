3bc7da9902b892f4bf465d4b30904641b7851cc6
vergil0327 <vergil.wang@relajet.com>
2024-01-15 15:04:58 +0800

    feat: top-down dp - 2312. Selling Pieces of Wood

--- 2-D Dynamic Programming/2312. Selling Pieces of Wood/solution.py
+++ 2-D Dynamic Programming/2312. Selling Pieces of Wood/solution.py
@@ -14,3 +14,27 @@ class Solution:
                     dp[i][j] = max(dp[i][j], dp[i][j-cut] + dp[i][cut])
 
         return dp[m][n]
+
+# top-down dp (dfs + memorization)
+class Solution:
+    def sellingWood(self, m: int, n: int, prices: List[List[int]]) -> int:
+        sell = defaultdict(int)
+        for h, w, p in prices:
+            sell[h, w] = p
+
+        @cache
+        def dfs(height, width):
+            if height < 0 or width < 0: return -inf
+
+            # no cut
+            price = sell[height, width] if (height, width) in sell else 0
+            
+            # vertically cut
+            for w in range(1, width):
+                price = max(price, dfs(height, w) + dfs(height, width-w))
+            
+            # horizontally cut
+            for h in range(1, height):
+                price = max(price, dfs(h, width) + dfs(height-h, width))
+            return price
+        return dfs(m, n)
