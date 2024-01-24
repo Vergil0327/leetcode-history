2bc02bdcd10c176f5a26accd2d8853082785b596
vergil0327 <vergil.wang@relajet.com>
2024-01-24 14:37:54 +0800

    feat: add bottom-up solution - 2172. Maximum AND Sum of Array

--- 2-D Dynamic Programming/Bitmask/2172. Maximum AND Sum of Array/solution.py
+++ 2-D Dynamic Programming/Bitmask/2172. Maximum AND Sum of Array/solution.py
@@ -20,3 +20,18 @@ class Solution:
 
             return res
         return dfs(0, 0)
+
+class Solution:
+    def maximumANDSum(self, nums: List[int], numSlots: int) -> int:
+        n = len(nums)
+
+        mxState = pow(3, numSlots)
+        dp = [[0]*mxState for _ in range(n+1)]
+        dp[0][0] = 0
+        for i in range(1, n+1):
+            for state in range(mxState):
+                for slot in range(1, numSlots+1):
+                    if (state//pow(3, slot-1)) % 3 == 0: continue
+                    dp[i][state] = max(dp[i][state], dp[i-1][state-pow(3, slot-1)] + (nums[i-1]&slot))
+
+        return dp[n][mxState-1]
