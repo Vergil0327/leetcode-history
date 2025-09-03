96b79090c61ec146b02faa88c243eb5f0e3d9274
vergil0327 <vergil.wang@relajet.com>
2025-09-03 09:22:47 +0800

    feat: add c++ solution to 3027. Find the Number of Ways to Place People II

---
+++ Sorted Container/SortedList/3027. Find the Number of Ways to Place People II/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), [](const auto &a, const auto &b) {
+            return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
+        });
+
+        int res = 0, n = points.size();
+        for (int i = 0; i < n; i++) {
+            long long y1 = points[i][1], last = -1e10;
+            for (int j = i + 1; j < n; j++) {
+                long long y2 = points[j][1];
+                if (y2 <= y1 && y2 > last) {
+                    last = y2;
+                    ++res;
+                }
+            }
+        }
+
+        return res;
+    }
+};
+
